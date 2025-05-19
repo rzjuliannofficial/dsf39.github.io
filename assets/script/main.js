44236405392a97970ac942d754d79c03cb16f8c2
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-05-19 21:46:50 +0700

    first move folder

---
+++ assets/script/main.js
@@ -0,0 +1,19 @@
+
+// JavaScript untuk mengelola interaksi navbar burger
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('header nav');
+
+    // Toggle class 'active' untuk menampilkan/menyembunyikan navbar
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+
+    // JavaScript untuk mengelola efek auto-typing
+    const typed = new Typed(".auto-typing", {
+        strings: ["FrontEnd Developer", "BackEnd Developer", "FullStack Developer"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true
+    });
+});
